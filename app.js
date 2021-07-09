e36ada3d59c3c04d969f9778e2c7339263f7db43
Justin Burks <burkstremane@gmail.com>
2021-07-09 05:15:54 -0400

    Test 2

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-
-
-app.get('/docs', (req,res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/loader.html'))
+})
+app.get('/dashboard', (req,res) => {
+    res.sendFile(path.join(__dirname, '/dashboard.html'));
 })
 
+app.get('/contact', (req,res) => {
+    res.sendFile(path.join(__dirname, '/contact.html'));
+})
+
+app.get('/docs', (req,res) => {
+    res.sendFile(path.join(__dirname, '/docs.html'));
+})
+
+app.get('/directory', (req,res) => {
+    res.sendFile(path.join(__dirname, '/directory.html'));
+})
 
 app.listen(3000, () => {
     console.log('I think its working boss');
